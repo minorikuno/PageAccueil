8371d2b8fec3f89575b3650abf206f29b77292b5
MinoriKUNO <minori.fr@gmail.com>
2021-05-10 17:22:40 +0200

    Added JS & style.css

---
+++ main.js
@@ -0,0 +1,6 @@
+let bear = document.getElementById('bearBtn');
+
+bear.addEventListener('click', ()=>{
+  let sec = document.getElementById('sec');
+  sec.style.backgroundImage = "url(../PageAccueil/src/img/ours_regard.jpg)";
+})
